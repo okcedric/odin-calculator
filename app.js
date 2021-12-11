4eede747e7d3c912456fd1c7fd59d1318392a2f3
pr0pr3 <salut@tchang.fr>
2021-12-11 12:50:11 +0100

    Build basic operations functions

---
+++ app.js
@@ -0,0 +1,12 @@
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => (a / b) ;
+
+let a = 5; 
+let b = 2;
+
+console.log(add(a,b));
+console.log(substract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
