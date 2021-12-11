217a98e69e46fcd2b269f7e1833774347f953243
pr0pr3 <salut@tchang.fr>
2021-12-11 17:10:04 +0100

    Populate displayValue in screen

--- app.js
+++ app.js
@@ -9,3 +9,43 @@ const operate = (operator,a,b) => {
     if (operator == "times")return multiply(a,b);
     if (operator == "obelus")return divide(a,b);
 }
+
+const expression = document.querySelector('.expression');
+const displayables = document.querySelectorAll('.operator, .number');
+const backspace = document.querySelector('#backspace');
+const clearButton = document.querySelector('#clear');
+
+//listen to numbers and operators buttons
+let displayValue = expression.textContent ;
+displayables.forEach(button => {
+    button.addEventListener('click', () => {
+        displayValue += button.textContent;
+        
+        updateScreen();
+    })
+})
+
+
+backspace.addEventListener('click', removeLast);
+
+
+function updateScreen() {
+    expression.textContent = displayValue;
+    return displayValue;
+}
+
+function clear() {
+    displayValue = "";
+    console.log('clear function : launched');
+    return updateScreen();
+}
+function removeLast() {
+   let testValue = displayValue.slice(0,-1);
+   displayValue =testValue;
+    console.log(testValue);
+    return updateScreen();
+}
+
+
+// store display value to a string variable called displayValue
+//make appear display value 
